a894d67e262063814b583c7cf82248ea3f443315
Siddharth Saxena <siddharthsaxena9101@gmail.com>
2026-04-03 21:03:14 +0530

    Added navbar & fixed fonts & colors

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import { Link } from "react-router";
+
+const NAV_LINKS = [
+  {
+    label: "gallery",
+    link: "#gallery",
+  },
+  {
+    label: "about",
+    link: "/about",
+  },
+  {
+    label: "contact",
+    link: "/contact",
+  },
+];
+
+const Navbar = () => {
+  return (
+    <header className="container">
+      <nav className="navbar">
+        {/* logo */}
+        <div>
+          <h2 className="logo">Woven</h2>
+        </div>
+
+        {/* nav links */}
+        <ul className="nav_links_container">
+          {NAV_LINKS.map((item, i) => (
+            <li key={i} className="nav_link">
+              <Link to={item.link}>{item.label}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
