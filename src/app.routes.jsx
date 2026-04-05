bcb0c1316c04944887d99e28302e987de40750c1
Siddharth Saxena <siddharthsaxena9101@gmail.com>
2026-04-05 11:39:55 +0530

    Added Scroll Restoration

--- src/app.routes.jsx
+++ src/app.routes.jsx
@@ -1,20 +1,32 @@
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter, ScrollRestoration, Outlet } from "react-router";
 import Home from "./pages/Home.jsx";
 import GalleryPage from "./pages/GalleryPage.jsx";
 import ProductPage from "./pages/ProductPage.jsx";
 
+const RootLayout = () => (
+  <>
+    <ScrollRestoration />
+    <Outlet />
+  </>
+);
+
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/gallery",
-    element: <GalleryPage />,
-  },
-  {
-    path: "/gallery/:title",
-    element: <ProductPage />,
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/gallery",
+        element: <GalleryPage />,
+      },
+      {
+        path: "/gallery/:title",
+        element: <ProductPage />,
+      },
+    ],
   },
 ]);
 
